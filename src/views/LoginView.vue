5fb989699fee93acfacccadd976d47a10ab77f9c
HsiaoCz <984274788@qq.com>
2023-12-31 08:46:23 +0800

    vue project

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,19 +1,78 @@
 <template>
     <div class="login-box">
-        登录页面
+        <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" label-width="80px" class="demo-ruleForm">
+            <h2>后台管理系统</h2>
+            <el-form-item label="账号" prop="username">
+                <el-input v-model="ruleForm.username" type="username" autocomplete="off" />
+            </el-form-item>
+            <el-form-item label="密码" prop="checkPass">
+                <el-input v-model="ruleForm.checkPass" type="password" autocomplete="off" />
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submitForm(ruleFormRef)">Submit</el-button>
+                <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import { defineComponent, reactive, ref } from 'vue'
+import type { FormInstance, FormRules } from 'element-plus';
 
-export default defineComponent({
-    setup() {
-
-
-        return {}
+const ruleFormRef = ref<FormInstance>()
+const validateUsername = (rule: any, value: any, callback: any) => {
+    if (value === '') {
+        callback(new Error('Please input the Username'))
+    } else {
+        if (ruleForm.username !== '') {
+            if (!ruleFormRef.value) return
+            ruleFormRef.value.validateField('username', () => null)
+        }
+        callback()
     }
+}
+const validatePass = (rule: any, value: any, callback: any) => {
+    if (value === '') {
+        callback(new Error('Please input the password'))
+    } else {
+        callback()
+    }
+}
+
+const ruleForm = reactive({
+    username: '',
+    checkPass: '',
 })
+const rules = reactive<FormRules<typeof ruleForm>>({
+    username: [{ validator: validateUsername, trigger: 'blur' }],
+    checkPass: [{ validator: validatePass, trigger: 'blur' }],
+})
+
+const submitForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    formEl.validate((valid) => {
+        if (valid) {
+            console.log('submit!')
+        } else {
+            console.log('error submit!')
+            return false
+        }
+    })
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    formEl.resetFields()
+}
+// export default defineComponent({
+//     setup() {
+//   const data=reactive({
+//
+// })
+//         return {}
+//     }
+// })
 </script>
 
 <style lang="scss" scoped>
@@ -21,5 +80,19 @@ export default defineComponent({
     width: 100%;
     height: 100%;
     background: url("../assets/loginbg.jpg");
+    padding: 1px;
+    text-align: center;
+
+    .demo-ruleForm {
+        width: 500px;
+        margin: 200px auto;
+        background-color: white;
+        padding: 40px;
+        border-radius: 20px;
+    }
+
+    h2 {
+        margin-bottom: 10px;
+    }
 }
 </style>
